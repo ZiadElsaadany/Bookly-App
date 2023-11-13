a434c8346c5a9daa465848bb4c2f0ada5152b97a
ziad elsaadany <ziadelsaadany73@gmail.com>
2023-11-13 15:36:11 +0200

    implement feature books method

--- lib/features/home/data/repos/home_repo_impementation.dart
+++ lib/features/home/data/repos/home_repo_impementation.dart
@@ -38,8 +38,27 @@ if(e is DioException){
   }
 
   @override
-  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() async{
+    try {
+      var data =await apiService.get(endPoint: "?Filtering=free-ebooks&q=subject:Programming");
+      List<BookModel> books =[ ];
+      for(var item in data["items"]){
+        books.add(BookModel.fromJson(item));
+      }
+
+      return right(books);
+    }
+    catch(e) {
+      if(e is DioException){
+
+        return left(ServerFailure.fromDioError(e));
+
+      }
+      return left(ServerFailure(
+          e.toString()
+      ));
+
+    }
   }
 
 }
