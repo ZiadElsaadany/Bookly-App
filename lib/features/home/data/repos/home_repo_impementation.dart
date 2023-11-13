a7c4cbfa5a23ac26e7468fb228a0fe4c51a6a19d
ziad elsaadany <ziadelsaadany73@gmail.com>
2023-11-13 14:50:26 +0200

    implement Newest= Best Seller books

--- lib/features/home/data/repos/home_repo_impementation.dart
+++ lib/features/home/data/repos/home_repo_impementation.dart
@@ -1,12 +1,31 @@
 import 'package:bookly/core/errors/failure.dart';
+import 'package:bookly/core/utils/api_service.dart';
 import 'package:bookly/features/home/data/models/book_model/BookModel.dart';
 import 'package:bookly/features/home/data/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
 class HomeRepoImplementation implements HomeRepo {
+
+  final ApiService apiService ;
+  HomeRepoImplementation(
+  {
+    required this.apiService
+}
+      );
   @override
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() async {
+try {
+  var data =await apiService.get(endPoint: "?Filtering=free-ebooks&q=subject:Programming&Sorting=newest");
+  List<BookModel> books =[ ];
+   for(var item in data["items"]){
+     books.add(BookModel.fromJson(item));
+   }
+
+   return right(books);
+}catch(e) {
+  return left(ServerFailure());
+
+}
   }
 
   @override
