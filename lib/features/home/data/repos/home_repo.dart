9e53559855f6481ccdd49a2fd786b7dc5f2ea16f
ziad elsaadany <ziadelsaadany73@gmail.com>
2023-11-13 14:12:35 +0200

    add repository pattern

---
+++ lib/features/home/data/repos/home_repo.dart
@@ -0,0 +1,21 @@
+
+// Repository Pattern
+
+// abstract class And All methods for this feature
+import 'package:bookly/features/home/data/models/book_model/BookModel.dart';
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/errors/failure.dart';
+
+abstract class HomeRepo {
+
+// get bestSeller Books
+// get Feature Books
+// determine methods (names only not how)
+
+Future<Either< Failure ,List<BookModel>>> fetchBestSellerBooks( ) ;
+Future<Either< Failure ,List<BookModel>>> fetchFeaturedBooks( ) ;
+
+
+
+}
