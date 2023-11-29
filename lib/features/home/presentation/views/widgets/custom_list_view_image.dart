2a9438fe16cbb21857a7af07d9eb90815d05e34c
ziad elsaadany <ziadelsaadany73@gmail.com>
2023-11-29 14:18:20 +0200

    cached network image package added

--- lib/features/home/presentation/views/widgets/custom_list_view_image.dart
+++ lib/features/home/presentation/views/widgets/custom_list_view_image.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 class CustomImageForBook extends StatelessWidget {
   const CustomImageForBook({Key? key, required this.imageUrl}) : super(key: key);
@@ -5,16 +6,26 @@ class CustomImageForBook extends StatelessWidget {
   final String imageUrl ;
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 3/4,
-      child: Container(
-        margin: const EdgeInsets.only(right: 12),
-        decoration:BoxDecoration(
-            borderRadius: BorderRadius.circular(15),
-            image:  DecorationImage(
-                fit: BoxFit.fill,
-                image: NetworkImage(imageUrl)
-            )
+    return Padding(
+      padding: const EdgeInsets.only(
+          right: 16.0),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(16),
+        child: AspectRatio(
+          aspectRatio: 3/4,
+          child: CachedNetworkImage(
+            imageUrl: imageUrl,
+            fit: BoxFit.cover,
+            errorWidget: (c,s,r) {
+              return const Icon(Icons.error);
+            },
+            placeholder: (c,d) {
+              return const Center(child: CircularProgressIndicator());
+            } ,
+
+
+
+          ),
         ),
       ),
     );
